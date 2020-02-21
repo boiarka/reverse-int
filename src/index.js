3041ac81c8e2e0714d2a987e81f246ba540d8c4d
Yurii <yurii.androsiuk@gmail.com>
2020-02-21 17:39:18 +0200

    feat: func working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let res;
+    if (String(n)[0] === '-') {
+        res = String(n).match(/(\d+)/)[0].split('').reverse().join('');
+    } else {
+        res = String(n).split('').reverse().join('');
+    }
+
+    return res;
 }
